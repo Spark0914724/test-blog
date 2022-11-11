ce5958fb19aab4e6df5c53fda930940dffb115ad
Spark0914724 <sophia.Cls@outlook.com>
2022-11-11 09:00:00 +0300

    add sort direction

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -30,14 +30,24 @@ final class CategoryController
 
         $perPage = $this->config['app']['per_page'];
         $page = max(1, (int) ($_GET['p'] ?? 1));
-        $sort = ($_GET['sort'] ?? 'date') === 'views' ? 'views' : 'date';
+
+        $rawSort = $_GET['sort'] ?? null;
+        if ($rawSort === null || $rawSort === '') {
+            // No sort specified: default to views
+            $sort = 'views';
+        } else {
+            // Explicit sort: only 'views' or 'date' are allowed; anything else falls back to date
+            $sort = $rawSort === 'views' ? 'views' : 'date';
+        }
+        $dirParam = strtolower((string) ($_GET['dir'] ?? 'desc'));
+        $direction = $dirParam === 'asc' ? 'asc' : 'desc';
 
         $total = $articleRepo->countByCategoryId($id);
         $totalPages = $total > 0 ? (int) ceil($total / $perPage) : 1;
         $page = min($page, $totalPages);
         $offset = ($page - 1) * $perPage;
 
-        $articles = $articleRepo->getByCategoryId($id, $perPage, $offset, $sort);
+        $articles = $articleRepo->getByCategoryId($id, $perPage, $offset, $sort, $direction);
 
         $this->smarty->assign('category', $category);
         $this->smarty->assign('articles', $articles);
@@ -45,6 +55,7 @@ final class CategoryController
         $this->smarty->assign('totalPages', $totalPages);
         $this->smarty->assign('total', $total);
         $this->smarty->assign('sort', $sort);
+        $this->smarty->assign('direction', $direction);
         $this->smarty->assign('pageTitle', $category['title']);
         $this->smarty->display('category.tpl');
     }
