05ded6412253eb89dc243cecefcd22fcd1dbb6ca
Spark0914724 <sophia.Cls@outlook.com>
2022-11-11 09:00:00 +0300

    update categoryRepository

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-
+use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use PDO;
 use Smarty\Smarty;
@@ -17,4 +17,35 @@ final class CategoryController
         private array $config
     ) {}
 
+    public function show(int $id): void
+    {
+        $categoryRepo = new CategoryRepository($this->pdo);
+        $articleRepo = new ArticleRepository($this->pdo);
+
+        $category = $categoryRepo->getById($id);
+        if ($category === null) {
+            header('Location: index.php?page=home', true, 302);
+            exit;
+        }
+
+        $perPage = $this->config['app']['per_page'];
+        $page = max(1, (int) ($_GET['p'] ?? 1));
+        $sort = ($_GET['sort'] ?? 'date') === 'views' ? 'views' : 'date';
+
+        $total = $articleRepo->countByCategoryId($id);
+        $totalPages = $total > 0 ? (int) ceil($total / $perPage) : 1;
+        $page = min($page, $totalPages);
+        $offset = ($page - 1) * $perPage;
+
+        $articles = $articleRepo->getByCategoryId($id, $perPage, $offset, $sort);
+
+        $this->smarty->assign('category', $category);
+        $this->smarty->assign('articles', $articles);
+        $this->smarty->assign('currentPage', $page);
+        $this->smarty->assign('totalPages', $totalPages);
+        $this->smarty->assign('total', $total);
+        $this->smarty->assign('sort', $sort);
+        $this->smarty->assign('pageTitle', $category['title']);
+        $this->smarty->display('category.tpl');
+    }
 }
